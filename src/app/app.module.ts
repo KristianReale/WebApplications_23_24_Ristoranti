d661629946965dd79d89da52ba2d65ca3fbff571
Kristian <kristian.reale@gmail.com>
2023-12-07 11:43:37 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ChiSiamoComponent
   ],
   imports: [
     BrowserModule,
