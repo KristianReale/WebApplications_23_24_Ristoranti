4b40ab5c05c38b7a94b7b123c20adb4dfc6b80ed
Kristian <kristian.reale@gmail.com>
2023-12-07 12:46:07 +0100

    chiamata rest get

--- src/app/ristoranti-service.service.ts
+++ src/app/ristoranti-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from  '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ristorante } from './model/ristorante';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RistorantiServiceService {
+  private backendUrl = "http://localhost:8080"
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+  dammiRistorantiMigliori():Observable<Ristorante[]>{
+    return this.http.get<Ristorante[]>(this.backendUrl + "/ristorantiMigliori")
+  }
 }
